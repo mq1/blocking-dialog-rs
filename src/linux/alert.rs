a11674c3eb55dbb3d40711d1dfedcc18d7d7970b
Manuel Quarneti <mq1@ik.me>
2026-02-16 16:55:34 +0100

    feat: add initial linux code

---
+++ src/linux/alert.rs
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2026 Manuel Quarneti <mq1@ik.me>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use crate::{BlockingAlertDialog, BlockingDialogError, BlockingDialogLevel};
+use raw_window_handle::RawWindowHandle;
+
+fn get_level_flag(level: BlockingDialogLevel) -> &'static str {
+    match level {
+        BlockingDialogLevel::Info => "--info",
+        BlockingDialogLevel::Warning => "--warning",
+        BlockingDialogLevel::Error => "--error",
+    }
+}
+
+impl<'a> BlockingAlertDialog<'a> {
+    pub fn show(&self) -> Result<(), BlockingDialogError> {
+        let level_flag = get_level_flag(self.level);
+
+        let cmd = Command::new("zenity")
+            .arg(level_flag)
+            .arg("--title")
+            .arg(self.title)
+            .arg("--text")
+            .arg(self.message);
+
+        let _ = cmd.status();
+
+        Ok(())
+    }
+}
--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -1,2 +1,4 @@
 // SPDX-FileCopyrightText: 2026 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: MIT OR Apache-2.0
+
+pub mod alert;
