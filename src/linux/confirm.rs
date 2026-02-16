c86e10add6252ef4d6b06d22789b0956b68c887e
Manuel Quarneti <mq1@ik.me>
2026-02-16 18:02:30 +0100

    fix

--- src/linux/confirm.rs
+++ src/linux/confirm.rs
@@ -1,13 +1,11 @@
 // SPDX-FileCopyrightText: 2026 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use crate::{BlockingConfirmDialog, BlockingDialogError, BlockingDialogLevel};
+use crate::{BlockingConfirmDialog, BlockingDialogError};
 use std::process::Command;
 
 impl<'a> BlockingConfirmDialog<'a> {
     pub fn show(&self) -> Result<bool, BlockingDialogError> {
-        let level_flag = get_level_flag(self.level);
-
         let status = Command::new("zenity")
             .arg("--question")
             .arg("--title")
@@ -16,6 +14,6 @@ impl<'a> BlockingConfirmDialog<'a> {
             .arg(self.message)
             .status()?;
 
-        status == 0
+        status.success()
     }
 }
