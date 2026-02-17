521a9d5272eafebf8aee10710f2a23f0adcd10d4
Manuel Quarneti <mq1@ik.me>
2026-02-17 22:15:09 +0100

    kdialog fixes

--- src/linux/pick_files.rs
+++ src/linux/pick_files.rs
@@ -8,7 +8,12 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn parse_multi_select(raw: &str) -> Vec<PathBuf> {
-    raw.split('\n').map(PathBuf::from).collect()
+    raw.lines()
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(PathBuf::from)
+        .filter(|p| p.exists())
+        .collect()
 }
 
 fn get_kdialog_filter(filter: &[BlockingPickFilesDialogFilter]) -> String {
@@ -45,15 +50,8 @@ impl<'a, W: HasWindowHandle> BlockingPickFilesDialog<'a, W> {
 
             let output = Command::new("kdialog").args(args).output()?;
             let stdout = String::from_utf8_lossy(&output.stdout);
-            let trimmed = stdout.trim();
 
-            if trimmed.is_empty() {
-                Ok(Vec::new())
-            } else if self.multiple {
-                Ok(parse_multi_select(trimmed))
-            } else {
-                Ok(vec![PathBuf::from(trimmed)])
-            }
+            Ok(parse_multi_select(stdout.as_ref()))
         } else {
             let mut filter_args = Vec::new();
 
@@ -82,15 +80,8 @@ impl<'a, W: HasWindowHandle> BlockingPickFilesDialog<'a, W> {
 
             let output = Command::new("zenity").args(args).output()?;
             let stdout = String::from_utf8_lossy(&output.stdout);
-            let trimmed = stdout.trim();
 
-            if trimmed.is_empty() {
-                Ok(Vec::new())
-            } else if self.multiple {
-                Ok(parse_multi_select(trimmed))
-            } else {
-                Ok(vec![PathBuf::from(trimmed)])
-            }
+            Ok(parse_multi_select(stdout.as_ref()))
         }
     }
 }
