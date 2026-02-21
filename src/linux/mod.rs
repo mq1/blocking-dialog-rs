d9ad483e3c3d6e0442aae277723ad65a4d6e3af1
Manuel Quarneti <mq1@ik.me>
2026-02-21 13:20:08 +0100

    refactor!: remove message dialogs on linux

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Manuel Quarneti <mq1@ik.me>
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-pub mod alert;
-pub mod confirm;
 pub mod pick_directory;
 pub mod pick_files;
 pub mod save_file;
